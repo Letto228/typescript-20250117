c21c622c7990de9fcfa783c1ce072e0c8884cec7
LearnJS <learnjs@MacBook-Pro-Egor.local>
2026-01-18 18:56:09 +0300

    feat: day 2

--- dist/day-1/02-type-alias.js
+++ dist/day-1/02-type-alias.js
@@ -1,53 +1,43 @@
+"use strict";
 // type Size = 's' | 'm' | 'l';
-
 // function calculateButtonSize(size: Size) {}
-
 // let size: Size = 'm';
-
 // type Movie = {
 //     title: string;
 //     director: string;
 //     awards?: string[];
 // }
-
 // type Movie = {
 //     name: string;
 // }
-
 // type Film = {
 //     title: string;
 //     director: string;
 //     awards?: string[];
 //     duration: number;
 // }
-
 // type TvSeries = {
 //     title: string;
 //     director: string;
 //     awards?: string[];
 //     seasonCount: number;
 // }
-
 // type Played = {
 //     // title: number;
 //     // duration: number | string
 //     paly(): void;
 // }
-
 // type Film = Movie & Played & {
 //     duration: number;
 // }
-
 // type TvSeries = Movie & Played & {
 //     seasonCount: number;
 // }
-
 // const film: Film = {
 //     title: '1',
 //     director: '12',
 //     duration: 123,
 //     paly() {
-        
 //     },
 // }
 // const newFilm: Film = {
@@ -56,23 +46,18 @@
 //     duration: 321,
 //     awards: ['Global'],
 //     paly() {
-        
 //     },
 // }
-
 // const serials: TvSeries = {
 //     title: 'serials',
 //     director: 'direcrtor',
 //     seasonCount: 3,
 //     paly() {
-        
 //     },
 // }
-
 // interface Movie {
 //     name: string;
 // }
-
 // const movie: Movie = {
 //     title: '',
 //     director: '',
---
+++ dist/day-2/08-enums.js
@@ -0,0 +1,12 @@
+"use strict";
+// enum Size {
+//     s,
+//     m = 10,
+//     l,
+// }
+const size = "Medium" /* Size.m */;
+// const size: Size = 'Medium';
+function getButtonSize(size) { }
+getButtonSize("Long" /* Size.l */);
+getButtonSize('Long');
+const keys = Size['Medium'];
